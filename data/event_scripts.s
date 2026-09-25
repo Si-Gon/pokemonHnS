fa8babe0d302d836fc951ba3921b438f2431bb46
Si-Gon <267122766+Si-Gon@users.noreply.github.com>
2026-09-26 00:45:39 +0100

    event_scripts.s: cartel generico de TV en espanol (MAMA/PAPA podria querer ver...)

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -843,7 +843,7 @@ gText_PokemonCenterSign::
 	.string "POKéMON CENTER$"
 
 gText_MomOrDadMightLikeThisProgram::
-	.string "{STR_VAR_1} might like this program.\n"
+	.string "A {STR_VAR_1} le gustaría este programa.\n"
 	.string "… … … … … … … … … … … … … … … …\p"
 	.string "Better get going!$"
 
